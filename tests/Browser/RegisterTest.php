2905ba6508c4a773a531e068ca75ae0d241a7b66
Janek <janbarn00@gmail.com>
2026-04-09 20:38:12 +0200

    test: add auth tests

---
+++ tests/Browser/RegisterTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Auth;
+
+it('registers a user', function () {
+    visit('/register')
+        ->fill('name', 'John Doe')
+        ->fill('email', 'johndoe@gmail.com')
+        ->fill('password', 'password123!@#')
+        ->click('Create Account')
+        ->assertPathIs('/');
+
+    $this->assertAuthenticated();
+
+    expect(Auth::user())->toMatchArray([
+        'name' => 'John Doe',
+        'email' => 'johndoe@gmail.com'
+    ]);
+});
